90ad63f96f0fb8a110c987cec7621277a656005e
MColombo00 <48252195+MColombo00@users.noreply.github.com>
2024-03-13 01:49:17 +0000

    added documentation to python file

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-from flask import Flask, render_template
-import random
+from flask import Flask, render_template #import flask and render template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')#using the default route to render the startMenu html file
 def greeting():
     return render_template('startMenu.html')
 
 
-@app.route('/add/<num1>/<num2>')
+@app.route('/add/<num1>/<num2>') #uses the /add route to add two numbers
 def add(num1, num2):
-    return str(int(num1)+int(num2))
+    return str(int(num1)+int(num2))#converts strings to integers and adds
     
-@app.route('/sub/<num1>/<num2>')
+@app.route('/sub/<num1>/<num2>') #uses the /sub route to subtract two  numbers
 def sub(num1, num2):
     return str(int(num1)-int(num2))
     
-@app.route('/multi/<num1>/<num2>')
+@app.route('/multi/<num1>/<num2>') #uses the /multi route to multiply two  numbers
 def multi(num1, num2):
     return str(int(num1)*int(num2))
     
-@app.route('/div/<num1>/<num2>')
+@app.route('/div/<num1>/<num2>') #uses the /div route to divide two numbers
 def div(num1, num2):
     return str(int(num1)/int(num2))
